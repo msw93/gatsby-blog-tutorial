de13cb6cc0d87f13e67ac9fa201616d8bfa37edc
mikewiner <winer.mike@gmail.com>
2020-07-25 16:55:54 -0400

    blog next and previous

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 
-const Template = (props) => {
-    console.log(props)
-    const title = props.data.markdownRemark.frontmatter.title;
-    const html = props.data.markdownRemark.html;
+const Template = ({data, pageContext}) => {
+    console.log(pageContext)
+    const title = data.markdownRemark.frontmatter.title;
+    const html = data.markdownRemark.html;
+
+    //const prev = props.pathContext.prev.frontmatter.path === null ? '#' : props.pathContext.prev.frontmatter.path;
+
+    // if (props.pathContext.prev.frontmatter.path){
+    //     const prev = props.pathContext.prev.frontmatter.path
+    // }
+    // else{
+    //     const prev = '#'
+    // }
 
     return (
         <div>
             <h1 className="blog-title">{title}</h1>
             <div className='blog-post' dangerouslySetInnerHTML={{__html:html}}/>
+            <div>
+                <a href={``}>prev</a><a href={``}>next</a>
+            </div>
         </div>
     )
 }
